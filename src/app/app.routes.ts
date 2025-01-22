9cce460b7bb0d8584d4eb4507e31f69c7e6cc983
esetaro2 <bbot3333@gmail.com>
2025-01-22 11:15:50 +0100

    Add global styles, routing for login/register, and update index.html structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
+import { RegisterComponent } from './component/register/register.component';
+import { AuthGuard } from './guard/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [AuthGuard],
+    data: {role: 'ADMIN'},
+  },
+];
