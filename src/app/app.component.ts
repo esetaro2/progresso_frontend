1ed15c8ad044b124e404f9d742aa2593041e55da
esetaro2 <bbot3333@gmail.com>
2025-02-04 18:29:12 +0100

    update app component spec, app component ts, app routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { AuthService } from './service/auth.service';
-import { UserService } from './service/user.service';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
-  title = 'frontend';
-
-  constructor(private authService: AuthService, private userService: UserService) {}
-
-  ngOnInit(): void {
-    const token = this.authService.getToken();
-    if (token) {
-      const userId = this.authService.getUserId();
-      if (userId) {
-        this.userService.initializeUser(userId);
-      }
-    }
-  }
+export class AppComponent {
+  title = 'Progresso';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,31 +3,29 @@ import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { AuthGuard } from './guard/auth.guard';
 import { ProjectDashboardComponent } from './component/project-dashboard/project-dashboard.component';
+import { NotAuthorizedComponent } from './component/not-authorized/not-authorized.component';
+import { LoginGuard } from './guard/login.guard';
 
 export const routes: Routes = [
   {
-    path: 'login',
-    component: LoginComponent,
-  },
-  {
-    path: 'register',
-    component: RegisterComponent,
-    canActivate: [AuthGuard],
-    data: { role: 'ADMIN' },
-  },
-  {
-    path: 'dashboard/projects',
+    path: '',
     component: ProjectDashboardComponent,
     canActivate: [AuthGuard],
     data: { roles: ['ADMIN', 'TEAMMEMBER', 'PROJECTMANAGER'] },
   },
   {
-    path: '',
-    redirectTo: '/dashboard/projects',
-    pathMatch: 'full',
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [LoginGuard]
   },
   {
-    path: '**',
-    redirectTo: '/login',
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [AuthGuard],
+    data: { roles: ['ADMIN'] },
+  },
+  {
+    path: 'not-authorized',
+    component: NotAuthorizedComponent,
   },
 ];
