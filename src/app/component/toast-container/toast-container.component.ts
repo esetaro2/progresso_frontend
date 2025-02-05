7eb03b05444668ab6259f0ec0c118cc122f442df
esetaro2 <bbot3333@gmail.com>
2025-02-05 16:53:42 +0100

    add toast container component

---
+++ src/app/component/toast-container/toast-container.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { ToastService } from '../../service/toast.service';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-toast-container',
+  imports: [NgbModule, CommonModule, FormsModule, ReactiveFormsModule],
+  template: `
+    <ngb-toast
+      *ngFor="let toast of toastService.toasts"
+      [header]="toast.header!"
+      [class]="toast.classname"
+      [autohide]="true"
+      [delay]="toast.delay || 5000"
+      (hide)="toastService.remove(toast)"
+    >
+      {{ toast.text }}
+    </ngb-toast>
+  `,
+  styles: [
+    `
+      ngb-toast {
+        position: fixed;
+        top: 1rem;
+        right: 1rem;
+        z-index: 1200;
+      }
+    `,
+  ],
+})
+export class ToastContainerComponent {
+  constructor(public toastService: ToastService) {}
+}
