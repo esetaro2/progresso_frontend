fbad0636e531b3ae53859776801c6b6864267005
esetaro2 <bbot3333@gmail.com>
2025-03-04 10:16:46 +0100

    feat(app): add sidebar and back-to-top button components, implement authentication check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ToastContainerComponent } from "./component/toast-container/toast-container.component";
+import { ToastContainerComponent } from './component/toast-container/toast-container.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { AuthService } from './service/auth.service';
+import { CommonModule } from '@angular/common';
+import { BackToTopButtonComponent } from "./component/back-to-top-button/back-to-top-button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastContainerComponent],
+  imports: [
+    RouterOutlet,
+    ToastContainerComponent,
+    SidebarComponent,
+    CommonModule,
+    BackToTopButtonComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Progresso';
+
+  isAuthenticated = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.authService.isAuthenticated$.subscribe(
+      (authStatus) => (this.isAuthenticated = authStatus)
+    );
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { LoginComponent } from './component/login/login.component';
 import { RegisterComponent } from './component/register/register.component';
 import { AuthGuard } from './guard/auth.guard';
 import { ProjectDashboardComponent } from './component/project-dashboard/project-dashboard.component';
-import { NotAuthorizedComponent } from './component/not-authorized/not-authorized.component';
 import { LoginGuard } from './guard/login.guard';
 import { ProjectPageComponent } from './component/project-page/project-page.component';
 import { AdminDashboardComponent } from './component/admin-dashboard/admin-dashboard.component';
@@ -31,10 +30,6 @@ export const routes: Routes = [
     component: ProjectPageComponent,
     canActivate: [AuthGuard],
     data: { roles: ['ADMIN', 'TEAMMEMBER', 'PROJECTMANAGER'] },
-  },
-  {
-    path: 'not-authorized',
-    component: NotAuthorizedComponent,
   },
   {
     path: 'admin/dashboard',
