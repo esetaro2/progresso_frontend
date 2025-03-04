0958c07fd33ffddad5fd87c1f17a349be0cc4375
esetaro2 <bbot3333@gmail.com>
2025-03-04 10:31:10 +0100

    feat(dto): enhance ProjectDto with additional fields and add user change password and update DTOs

--- src/app/dto/project.dto.ts
+++ src/app/dto/project.dto.ts
@@ -1,15 +1,19 @@
 export interface ProjectDto {
-    id?: number;
-    name: string;
-    description: string;
-    priority?: string;
-    startDate: string; 
-    dueDate: string;
-    completionDate?: string;
-    status?: string;
-    projectManagerId: number;
-    taskIds?: number[];
-    teamId?: number;
-    commentIds?: number[];
-  }
-  
+  id?: number;
+  name: string;
+  description: string;
+  priority?: string;
+  startDate: string;
+  dueDate: string;
+  completionDate?: string;
+  completionPercentage?: number;
+  status?: string;
+  projectManagerId: number;
+  projectManagerFirstName?: string;
+  projectManagerLastName?: string;
+  projectManagerUsername?: string;
+  taskIds?: number[];
+  teamId?: number;
+  teamName?: string;
+  commentIds?: number[];
+}
